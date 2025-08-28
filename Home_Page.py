50a80399fda05590786dd86af9537881520c655a
gannamansour <gannamansour04@gmail.com>
2025-08-28 18:38:15 +0300

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit_option_menu import option_menu  
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+
+st.write("Data Dashboard For California Houses")
+
+    # Output in Web Streamlit :
+@contextmanager
+def st_capture(output_func):
+                with StringIO() as stdout, redirect_stdout(stdout):
+                    old_write = stdout.write
+
+                    def new_write(string):
+                        ret = old_write(string)
+                        output_func(stdout.getvalue())
+                        return ret
+                    
+                    stdout.write = new_write
+                    yield
+
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="User's Input For Data Preparation",
+        options=[
+            "California Houses",
+            "Telescope Data",
+        ],
+    )
+
+if selected == "Telescope Data":
+       st.title("Dashboard For Telescope Data")
+       data = pd.read_csv("Telescope_data.csv")
+       data = pd.DataFrame(data)
+       st.write("You selected All Data")
+       st.dataframe(data)
+
+if selected == "California Houses":
+       st.title("Dashboard For California Houses")
+       data = pd.read_csv("California_Houses.csv")
+       data = pd.DataFrame(data)
+       st.write("You selected All Data")
+       st.dataframe(data)       
---
+++ README.md
@@ -0,0 +1,10 @@
+# Knn-LinearRegression
+Real World example form we learned to do life tasks or make life easier !!
+## get started
+enter the folder
+```cd Knn-LinearRegression```
+wright this command in the terminal to download all the packages
+``` pip install -r requirements.txt ```
+
+then to run the app
+```streamlit run Home_Page.py ```
